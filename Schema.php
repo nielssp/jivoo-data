b1a3bea3ff947a7b58c4348bfd9ee0b256d9d9b2
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-11-02 18:31:57 +0100

    Redo schema interface.

--- Schema.php
+++ Schema.php
@@ -3,7 +3,7 @@
 // Copyright (c) 2015 Niels Sonnich Poulsen (http://nielssp.dk)
 // Licensed under the MIT license.
 // See the LICENSE file or http://opensource.org/licenses/MIT for more information.
-namespace Jivoo\Data\Definition;
+namespace Jivoo\Data;
 
 /**
  * Model schema.
@@ -17,9 +17,16 @@ interface Schema {
   
   /** 
    * Get list of fields.
-   * @return Field[] List of fields.
+   * @return string[] List of field names.
    */
   public function getFields();
+  
+  /**
+   * Get type of field
+   * @param string $field Field name.
+   * @return DataType Type of field.
+   */
+  public function getType($field);
   
   /**
    * Get fields of primary key.
@@ -28,9 +35,23 @@ interface Schema {
   public function getPrimaryKey();
 
   /**
-   * Get indexes. 
-   * @return Key[] Keys.
+   * Get names of indexes/keys. 
+   * @return string[] Names of keys.
    */
   public function getKeys();
+  
+  /**
+   * Get fields of key.
+   * @param string $key Key name.
+   * @return string[] List of field names.
+   */
+  public function getKey($key);
+  
+  /**
+   * Whether key is unique.
+   * @param string $key Key name.
+   * @return bool True if unique, false otherwise.
+   */
+  public function isUnique($key);
   
 }
