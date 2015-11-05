d445eccf17148b5a6cf712d3cf8ec30ec055b6a3
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2015-11-05 16:57:46 +0100

    Add enum types to Data module.

---
+++ Enum.php
@@ -0,0 +1,112 @@
+<?php
+// Jivoo
+// Copyright (c) 2015 Niels Sonnich Poulsen (http://nielssp.dk)
+// Licensed under the MIT license.
+// See the LICENSE file or http://opensource.org/licenses/MIT for more information.
+namespace Jivoo\Data;
+
+use Jivoo\Core\Assume;
+
+/**
+ * Used for creating enum types.
+ * 
+ * Example:
+ * <code>
+ * class DayOfWeek extends Enum {
+ *   const monday = 1;
+ *   const tuesday = 2;
+ *   const wednesday = 3;
+ *   const thursday = 4;
+ *   const friday = 5;
+ *   const saturday = 6;
+ *   const sunday = 7;
+ * }
+ * </code>
+ * 
+ */
+abstract class Enum {
+  /**
+   * @var array Values of enums.
+   */
+  private static $values = array();
+
+  /**
+   * @var string[].
+   */
+  private static $classes = array();
+  
+  /**
+   * @var string[]
+   */
+  private static $searchPrefixes = array();
+
+  private final function __construct() { }
+
+  /**
+   * Whether an enum class exists.
+   * @param string $class Class name.
+   * @return bool True if class exists.
+   */
+  public static function classExists($class) {
+    if (isset(self::$values[$class]))
+      return true;
+    if (isset(self::$classes[$class]))
+      return true;
+    if (class_exists($class)) {
+      self::$classes[$class] = $class;
+      return true;
+    }
+    foreach (self::$searchPrefixes as $prefix) {
+      if (class_exists($prefix . $class)) {
+        self::$classes[$class] = $prefix . $class;
+        return true;
+      }
+    }
+    return false;
+  } 
+  
+  /**
+   * Get values of an enum class.
+   * @param string $class Class name.
+   * @throws InvalidEnumException If the class invalid or  does not contain
+   * constants.
+   * @return string[] Enum values.
+   */
+  public static function getValues($class = null) {
+    if (!isset($class))
+      $class = get_called_class();
+    if (!isset(self::$values[$class])) {
+      if (!self::classExists($class))
+        throw new InvalidEnumException(tr('Enum class not found: %1', $class));
+      $class = self::$classes[$class];
+      Assume::isSubclassOf($class, 'Jivoo\Models\Enum');
+      $ref = new \ReflectionClass($class);
+      self::$values[$class] = array_flip($ref->getConstants());
+      if (count(self::$values[$class]) < 1)
+        throw new InvalidEnumException(tr('Enum type "%1" must contain at least one constant', $class));
+    }
+    return self::$values[$class];
+  }
+  
+  /**
+   * Add namespace to search for enum classes under.
+   * @param string $prefix Prefix, e.g. 'App\Enums\\'
+   */
+  public static function addSearchPrefix($prefix) {
+    self::$searchPrefixes[] = $prefix;
+  }
+
+  /**
+   * Get index of an enum value.
+   * @param string $str Enum value.
+   * @param string $class Class name.
+   * @return int Index.
+   */
+  public static function getValue($str, $class = null) {
+    if (!isset($class))
+      $class = get_called_class();
+    if (!isset(self::$values[$class]))
+      self::getValues($class);
+    return array_search($str, self::$values[$class]);
+  }
+}
---
+++ EnumDataType.php
@@ -0,0 +1,92 @@
+<?php
+// Jivoo
+// Copyright (c) 2015 Niels Sonnich Poulsen (http://nielssp.dk)
+// Licensed under the MIT license.
+// See the LICENSE file or http://opensource.org/licenses/MIT for more information.
+namespace Jivoo\Data;
+
+use Jivoo\Models\Validation\ValidatorField;
+use Jivoo\InvalidPropertyException;
+
+/**
+ * The data type of {@see Enum}s.
+ * @property-read string[] $values Enum values.
+ */
+class EnumDataType extends DataType {
+  
+  /**
+   * @var string Enum class if any.
+   */
+  private $class = null;
+  
+  /**
+   * @var string[] Enum values.
+   */
+  private $values;
+  
+  /**
+   * Construct enum data type.
+   * @param string|string[] $valuesOrClass Either an enum class or a list of
+   * enum values.
+   * @param string $null Is nullable.
+   * @param string $default Default value.
+   * @throws InvalidEnumException If default values is not part of enum.
+   */
+  protected function __construct($valuesOrClass, $null = false, $default = null) {
+    parent::__construct(DataType::ENUM, $null, $default);
+    if (is_array($valuesOrClass)) {
+      $this->values = $valuesOrClass;
+    }
+    else { 
+      $this->class = $valuesOrClass;
+      $this->values = Enum::getValues($this->class);
+    }
+    if (isset($default) and !in_array($default, $this->values)) {
+      throw new InvalidEnumException(tr(
+        'Default value must be part of enum'
+      ));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __get($property) {
+    if ($property === 'values')
+      return $this->values;
+    if ($property === 'placeholder') {
+      if (!isset($this->class))
+        throw new InvalidPropertyException(tr('Invalid use of anonymous enum type'));
+      return '%' . $this->class;
+    }
+    return parent::__get($property);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __toString() {
+    if (!isset($this->class))
+      return 'Enum(' . implode(', ', $this->values) . ')';
+    return $this->class;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function createValidationRules(ValidatorField $validator) {
+    $validator = $validator->ruleDataType;
+    if (!$this->null)
+      $validator->null = false;
+    $validator->in = array_values($this->values);
+  }
+
+  /**
+   * {@inheritdoc}
+   */  
+  public function isValid($value) {
+    if ($this->null and $value == null)
+      return true;
+    return in_array($value, $this->values);
+  }
+}
