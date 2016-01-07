be9bddb2efe02a87db35579af59bf08f1c6788ea
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2016-01-07 19:59:24 +0100

    Add empty expresion parser class.

---
+++ Query/Expression/ExpressionParser.php
@@ -0,0 +1,23 @@
+<?php
+// Jivoo
+// Copyright (c) 2015 Niels Sonnich Poulsen (http://nielssp.dk)
+// Licensed under the MIT license.
+// See the LICENSE file or http://opensource.org/licenses/MIT for more information.
+namespace Jivoo\Data\Query\Expression;
+
+use Jivoo\Core\Parse\ParseInput;
+
+/**
+ * A parser for expressions.
+ */
+class ExpressionParser {
+
+
+  /**
+   * @param ParseInput $input
+   * @return ast
+   */
+  public static function parseExpression(ParseInput $input) {
+  }
+}
+
