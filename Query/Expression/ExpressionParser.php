473d62f65c21f4d9ba416240e407b3918a85d5d9
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2016-01-08 15:22:06 +0100

    Add simple lexer.

--- Query/Expression/ExpressionParser.php
+++ Query/Expression/ExpressionParser.php
@@ -14,10 +14,27 @@ class ExpressionParser {
 
 
   /**
+   * @param string $expression
+   * @return ParseInput
+   */
+  public static function scan($expression) {
+    $lexer = new RegexLexer(true, 'i');
+    $lexer->keyword = 'and|not|or|like';
+    $lexer->name = '[a-z][a-z0-9]+';
+    $lexer->float = '[0-9]+.[0-9]+';
+    $lexer->integer = '[0-9]+';
+    
+    return new ParseInput($lexer($expression));
+  }
+  
+  /**
    * @param ParseInput $input
    * @return ast
    */
   public static function parseExpression(ParseInput $input) {
+  }
+  
+  public static function parsePrefix(ParseInput $input) {
   }
 }
 
