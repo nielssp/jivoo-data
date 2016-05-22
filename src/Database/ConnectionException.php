e36cb19d9b0693c0dbc6b0f7019f1392f86ff8ce
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2016-05-22 17:30:03 +0200

    Add missing exception.

---
+++ src/Database/ConnectionException.php
@@ -0,0 +1,13 @@
+<?php
+// Jivoo
+// Copyright (c) 2015 Niels Sonnich Poulsen (http://nielssp.dk)
+// Licensed under the MIT license.
+// See the LICENSE file or http://opensource.org/licenses/MIT for more information.
+namespace Jivoo\Data\Database;
+
+/**
+ * Thrown when a database connection fails.
+ */
+class ConnectionException extends \RuntimeException implements \Jivoo\Exception
+{
+}
